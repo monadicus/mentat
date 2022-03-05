84f238f2230992205ff6046bd34806498bb528e5
mowkoshka <jp@monadic.us>
2022-03-04 18:59:35 -0800

    snarkos server, data api

--- rosetta-snarkos/server.rs
+++ rosetta-snarkos/server.rs
@@ -1,10 +1,11 @@
 use mentat::server::Server;
 use std::net::Ipv4Addr;
 
-
+mod data_api;
 
 #[rocket::main]
 async fn main() {
     let mut server = Server::new();
+    server.with_data_api(data_api::SnarkosDataApi{});
     server.serve(Ipv4Addr::new(127, 0, 0, 1), 3030).await;
 }
