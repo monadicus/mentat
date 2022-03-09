d9dca5d0045e575120471f5ee8c139bb694d05e3
MACS-J1149 <e@monadic.us>
2022-03-08 18:56:56 -0800

    added `/call` endpoint

--- src/api/data.rs
+++ src/api/data.rs
@@ -50,6 +50,9 @@ pub trait DataApi: Send + Sync {
     /// Get all Transaction Identifiers in the mempool
     async fn mempool(&self, caller: Caller, data: NetworkRequest) -> Response<MempoolResponse>;
 
+    /// Make a Network-Specific Procedure Call
+    async fn call(&self, _caller: Caller, data: CallRequest) -> Response<CallResponse>;
+
     /// Get a transaction in the mempool by its Transaction Identifier. This is a separate request than fetching a block transaction (/block/transaction) because some blockchain nodes need to know that a transaction query is for something in the mempool instead of a transaction in a block. Transactions may not be fully parsable until they are in a block (ex: may not be possible to determine the fee to pay before a transaction is executed). On this endpoint, it is ok that returned transactions are only estimates of what may actually be included in a block.
     async fn mempool_transaction(
         &self,
--- src/server/bitcoin_data.rs
+++ src/server/bitcoin_data.rs
@@ -257,6 +257,34 @@ impl DataApi for BitcoinDataApi {
         }
     }
 
+    async fn call(&self, _caller: Caller, data: CallRequest) -> Response<CallResponse> {
+        #[cfg(debug_assertions)]
+        log_payload("input  /call", serde_json::to_string(&data).unwrap());
+        let resp = match self
+            .client
+            .post(&format!("{}{}", self.url, "/call"))
+            .json(&data)
+            .send()
+            .await
+        {
+            Ok(resp) => resp,
+            Err(e) => {
+                return Err(match serde_json::from_str(&e.to_string()) {
+                    Ok(s) => MentatError::Internal(s),
+                    Err(_) => MentatError::from(format!("unhandled rosetta-bitcoin error: {}", e)),
+                })
+            }
+        };
+
+        let out = resp.text().await?;
+        #[cfg(debug_assertions)]
+        log_payload("output /call", &out);
+        match serde_json::from_str(&out) {
+            Ok(o) => Ok(Json(o)),
+            Err(_) => Err(MentatError::Internal(serde_json::from_str(&out)?)),
+        }
+    }
+
     async fn mempool(&self, _caller: Caller, data: NetworkRequest) -> Response<MempoolResponse> {
         #[cfg(debug_assertions)]
         log_payload("input  /mempool", serde_json::to_string(&data).unwrap());
