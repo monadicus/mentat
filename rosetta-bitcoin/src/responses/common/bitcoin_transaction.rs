177c3a65c5f6edee2650c04275cf0846cd46c0f0
MACS-J1149 <e@monadic.us>
2022-03-17 19:59:23 -0700

    progress

--- rosetta-bitcoin/src/responses/common/bitcoin_transaction.rs
+++ rosetta-bitcoin/src/responses/common/bitcoin_transaction.rs
@@ -1,154 +1,55 @@
-use mentat::{
-    identifiers::{AccountIdentifier, CoinIdentifier, OperationIdentifier, TransactionIdentifier},
-    models::{Amount, CoinAction, CoinChange, Currency, Operation, Transaction},
-    serde_json::{json, Value},
-    IndexMap,
-};
+use mentat::serde::Serialize;
 
 use super::*;
 
-// TODO there are different versions of vin...fml
-
-// #[derive(Clone, Debug, Deserialize)]
-// #[serde(crate = "mentat::serde")]
-// pub struct BitcoinVin {
-//     coinbase: String,
-//     sequence: usize,
-// }
-
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(crate = "mentat::serde")]
 pub struct BitcoinScriptSig {
-    asm: String,
-    hex: String,
+    pub asm: String,
+    pub hex: String,
 }
+
 #[allow(non_snake_case)]
 #[derive(Clone, Debug, Deserialize)]
 #[serde(crate = "mentat::serde")]
 pub struct BitcoinVin {
-    txid: String,
-    vout: usize,
-    scriptSig: BitcoinScriptSig,
-    sequence: usize,
-    txinwitness: Option<Vec<String>>,
+    pub txid: Option<String>,
+    pub vout: Option<u64>,
+    pub scriptSig: Option<BitcoinScriptSig>,
+    pub sequence: usize,
+    pub txinwitness: Option<Vec<String>>,
+    pub coinbase: Option<String>,
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(crate = "mentat::serde")]
 pub struct BitcoinScriptPubKey {
-    asm: String,
-    hex: String,
+    pub asm: String,
+    pub hex: String,
     #[serde(rename = "type")]
-    _type: String,
+    pub _type: String,
 }
 
 #[allow(non_snake_case)]
 #[derive(Clone, Debug, Deserialize)]
 #[serde(crate = "mentat::serde")]
 pub struct BitcoinVout {
-    value: f64,
-    n: u64,
-    scriptPubKey: BitcoinScriptPubKey,
+    pub value: f64,
+    pub n: u64,
+    pub scriptPubKey: BitcoinScriptPubKey,
 }
-
-// fn into_operation(vin: BitcoinVin, vout: BitcoinVout, txid: String, index: u64) -> [Operation; 2] {
-//     [
-//         Operation {
-//             operation_identifier: OperationIdentifier {
-//                 index: index * 2,
-//                 network_index: Some(vout.n),
-//             },
-//             related_operations: None,
-//             type_: "COINBASE".to_string(),
-//             status: Some("SUCCESS".to_string()),
-//             account: None,
-//             amount: None,
-//             coin_change: None,
-//             metadata: {
-//                 let mut map = IndexMap::new();
-//                 map.insert("coinbase".to_string(), vin.coinbase.into());
-//                 map.insert("sequence".to_string(), vin.sequence.into());
-//                 map
-//             },
-//         },
-//         Operation {
-//             operation_identifier: OperationIdentifier {
-//                 index: index * 2 + 1,
-//                 network_index: Some(vout.n),
-//             },
-//             related_operations: None,
-//             type_: "OUTPUT".to_string(),
-//             status: Some("SUCCESS".to_string()),
-//             account: Some(AccountIdentifier {
-//                 address: vout.scriptPubKey.hex.clone(),
-//                 decimals: None,
-//                 metadata: IndexMap::new(),
-//             }),
-//             amount: Some(Amount {
-//                 value: vout.value.to_string(),
-//                 currency: Currency {
-//                     symbol: "BTC".to_string(),
-//                     decimals: 8,
-//                     metadata: IndexMap::new(),
-//                 },
-//                 metadata: IndexMap::new(),
-//             }),
-//             coin_change: Some(CoinChange {
-//                 coin_identifier: CoinIdentifier { identifier: txid },
-//                 coin_action: CoinAction::CoinCreated,
-//             }),
-//             metadata: {
-//                 let mut inner_map: IndexMap<String, Value> = IndexMap::new();
-//                 inner_map.insert("asm".to_string(), vout.scriptPubKey.asm.into());
-//                 inner_map.insert("hex".to_string(), vout.scriptPubKey.hex.into());
-//                 inner_map.insert("type".to_string(), vout.scriptPubKey._type.into());
-//                 let mut map = IndexMap::new();
-//                 map.insert("scriptPubKey".to_string(), json!(inner_map));
-//                 map
-//             },
-//         },
-//     ]
-// }
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(crate = "mentat::serde")]
 pub struct BitcoinTransaction {
-    txid: String,
-    hash: String,
-    version: usize,
-    size: usize,
-    vsize: usize,
-    weight: usize,
-    // locktime: usize,
-    vin: Vec<BitcoinVin>,
-    vout: Vec<BitcoinVout>,
-    // hex: String,
-}
-
-impl From<BitcoinTransaction> for Transaction {
-    fn from(transaction: BitcoinTransaction) -> Self {
-        Transaction {
-            transaction_identifier: TransactionIdentifier {
-                hash: transaction.hash,
-            },
-            operations: transaction
-                .vin
-                .into_iter()
-                .zip(transaction.vout)
-                .enumerate()
-                .flat_map(|(i, (vin, vout))| {
-                    into_operation(vin, vout, transaction.txid.clone(), i as u64)
-                })
-                .collect(),
-            related_transactions: None,
-            metadata: {
-                let mut map = IndexMap::new();
-                map.insert("size".to_string(), transaction.size.into());
-                map.insert("version".to_string(), transaction.version.into());
-                map.insert("vsize".to_string(), transaction.vsize.into());
-                map.insert("weight".to_string(), transaction.weight.into());
-                map
-            },
-        }
-    }
+    pub txid: String,
+    pub hash: String,
+    pub version: usize,
+    pub size: usize,
+    pub vsize: usize,
+    pub weight: usize,
+    pub locktime: usize,
+    pub vin: Vec<BitcoinVin>,
+    pub vout: Vec<BitcoinVout>,
+    pub hex: String,
 }
--- rosetta-bitcoin/src/responses/data/getblock.rs
+++ rosetta-bitcoin/src/responses/data/getblock.rs
@@ -2,8 +2,14 @@ use crate::responses::common::BitcoinTransaction;
 
 use super::*;
 use mentat::{
-    api::MentatResponse, identifiers::BlockIdentifier, models::Block, responses::BlockResponse,
-    IndexMap, Json,
+    api::MentatResponse,
+    identifiers::{
+        AccountIdentifier, BlockIdentifier, CoinIdentifier, OperationIdentifier,
+        TransactionIdentifier,
+    },
+    models::{Block, CoinAction, CoinChange, Operation, Transaction},
+    responses::BlockResponse,
+    serde_json, IndexMap, Json,
 };
 
 #[allow(non_snake_case)]
@@ -44,7 +50,99 @@ impl From<GetBlockResponse> for MentatResponse<BlockResponse> {
                     hash: response.previousblockhash,
                 },
                 timestamp: response.time,
-                transactions: response.tx.into_iter().map(|t| t.into()).collect(),
+                transactions: response
+                    .tx
+                    .into_iter()
+                    .enumerate()
+                    .map(|(it, transaction)| Transaction {
+                        transaction_identifier: TransactionIdentifier {
+                            hash: transaction.hash.clone(),
+                        },
+                        operations: transaction
+                            .vin
+                            .into_iter()
+                            .enumerate()
+                            .map(|(io, vin)| Operation {
+                                operation_identifier: OperationIdentifier {
+                                    index: io as u64,
+                                    network_index: Some(vin.vout.unwrap_or(0)),
+                                },
+                                related_operations: None,
+                                type_: if it == 0 && io == 0 {
+                                    "COINBASE"
+                                } else {
+                                    "INPUT"
+                                }
+                                .to_string(),
+                                status: Some(String::from("SUCCESS")),
+                                account: vin.scriptSig.map(|s| AccountIdentifier {
+                                    address: s.hex,
+                                    decimals: None,
+                                    metadata: IndexMap::new(),
+                                }),
+                                amount: todo!(),
+                                coin_change: vin.txid.map(|id| CoinChange {
+                                    coin_identifier: CoinIdentifier { identifier: id },
+                                    coin_action: CoinAction::CoinSpent,
+                                }),
+                                metadata: {
+                                    let mut map = IndexMap::new();
+                                    if let Some(sig) = vin.scriptSig {
+                                        map.insert(
+                                            "scriptSig".to_string(),
+                                            serde_json::to_value(sig).unwrap(),
+                                        );
+                                    }
+                                    if let Some(c) = vin.coinbase {
+                                        map.insert("coinbase".to_string(), c.into());
+                                    }
+                                    map.insert("sequence".to_string(), vin.sequence.into());
+                                    map
+                                },
+                            })
+                            .chain(transaction.vout.into_iter().enumerate().map(|(io, vout)| {
+                                Operation {
+                                    operation_identifier: OperationIdentifier {
+                                        index: io as u64,
+                                        network_index: Some(vout.n),
+                                    },
+                                    related_operations: None,
+                                    type_: String::from("OUTPUT"),
+                                    status: Some(String::from("SUCCESS")),
+                                    account: Some(AccountIdentifier {
+                                        address: vout.scriptPubKey.hex,
+                                        decimals: None,
+                                        metadata: IndexMap::new(),
+                                    }),
+                                    amount: todo!(),
+                                    coin_change: Some(CoinChange {
+                                        coin_identifier: CoinIdentifier {
+                                            identifier: transaction.hash,
+                                        },
+                                        coin_action: CoinAction::CoinCreated,
+                                    }),
+                                    metadata: {
+                                        let mut map = IndexMap::new();
+                                        map.insert(
+                                            "scriptSig".to_string(),
+                                            serde_json::to_value(vout.scriptPubKey).unwrap(),
+                                        );
+                                        map
+                                    },
+                                }
+                            }))
+                            .collect(),
+                        related_transactions: None,
+                        metadata: {
+                            let mut map = IndexMap::new();
+                            map.insert("size".to_string(), transaction.size.into());
+                            map.insert("version".to_string(), transaction.version.into());
+                            map.insert("vsize".to_string(), transaction.vsize.into());
+                            map.insert("weight".to_string(), transaction.weight.into());
+                            map
+                        },
+                    })
+                    .collect(),
                 metadata: {
                     let mut map = IndexMap::new();
                     map.insert("bits".to_string(), response.bits.into());
