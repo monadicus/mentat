8a8d2f9a8af87cb191ac809f7d143b8714a38d1e
mowkoshka <99986223+mowkoshka@users.noreply.github.com>
2022-05-06 16:48:59 -0700

    [Impl] Generic Rpc call (#93)
    * initial commit, move logging setup teardown to server
    
    * add readme content back
    
    * turn rpc macro into a generic function
    
    * simplified generics
    
    * fmt
    
    * oops nightly fmt
    
    Co-authored-by: MACS-J1149 <e@monadic.us>

--- rosetta-snarkos/src/responses/response.rs
+++ rosetta-snarkos/src/responses/response.rs
@@ -1,19 +1,45 @@
-use mentat::serde::Deserialize;
+use std::fmt::Debug;
+
+use mentat::{
+    errors::Result,
+    serde::{de::DeserializeOwned, Deserialize},
+    server::RpcResponse,
+    tracing,
+};
 
 use super::ErrorResponse;
+use crate::request::SnarkosJrpc;
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(crate = "mentat::serde")]
-pub struct InnerResponse<R> {
+pub struct InnerResponse<I> {
     // jsonrpc: String,
-    pub result: R,
+    pub result: I,
     // id: String,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(crate = "mentat::serde")]
 #[serde(untagged)]
-pub enum Response<R> {
-    Ok(InnerResponse<R>),
+pub enum Response<O> {
+    Ok(InnerResponse<O>),
     Err(ErrorResponse),
+}
+
+impl<O> RpcResponse for Response<O>
+where
+    O: Debug + DeserializeOwned,
+{
+    type I = SnarkosJrpc;
+    type O = O;
+
+    fn unwrap_response(self) -> Result<Self::O> {
+        match self {
+            Response::Ok(res) => {
+                tracing::debug!("res: {res:#?}");
+                Ok(res.result)
+            }
+            Response::Err(err) => err.into(),
+        }
+    }
 }
