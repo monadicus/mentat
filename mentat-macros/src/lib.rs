5a758750a26a6dfe5e101de2051b8838ce38818c
MACS-J1149 <e@monadic.us>
2022-05-10 16:08:14 -0700

    moved main macros to own module

--- mentat-macros/src/lib.rs
+++ mentat-macros/src/lib.rs
@@ -1,111 +1,12 @@
 extern crate proc_macro;
 
-mod route_builder;
+mod main_gen;
 
+use main_gen::{get_cache_inner_type, gen_derive, gen_main, swap_main_name, get_function_return_server_type};
 use proc_macro::TokenStream;
-use proc_macro2::{Span, TokenStream as TokenStream2};
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{
-    self,
-    parse_macro_input,
-    AttributeArgs,
-    GenericArgument,
-    Ident,
-    ItemFn,
-    ItemStruct,
-    Meta,
-    NestedMeta,
-    PathArguments::{self},
-    PathSegment,
-    ReturnType,
-    Type,
-};
-
-/// Matches the provided macro argument for the optional `CacheInner` type.
-fn get_cache_inner_type(arg: &NestedMeta) -> Result<&Ident, TokenStream> {
-    if let NestedMeta::Meta(Meta::Path(path)) = arg {
-        if let Some(id) = path.get_ident() {
-            return Ok(id);
-        }
-    }
-    Err(
-        syn::Error::new(Span::call_site(), "expected type `CacheInner`")
-            .into_compile_error()
-            .into(),
-    )
-}
-
-/// Mutates the provided main function's name to `__mentat_main_server_call` so
-/// that it can be called from mentat's `main` function.
-fn swap_main_name(f: &mut ItemFn) -> Result<(), TokenStream> {
-    if f.sig.ident == "main" {
-        f.sig.ident = Ident::new("__mentat_main_server_call", f.sig.ident.span());
-        Ok(())
-    } else {
-        Err(syn::Error::new(
-            f.sig.ident.span(),
-            format!("expected function name `main` found `{}`", f.sig.ident),
-        )
-        .into_compile_error()
-        .into())
-    }
-}
-
-/// Matches the provided main function for the `Server`'s `ServerType`.
-fn get_function_return_server_type(f: &ItemFn) -> Result<&Ident, TokenStream> {
-    // TODO this is horribly nested but they don't provide any helper functions to
-    // avoid this
-    if let ReturnType::Type(_, t) = &f.sig.output {
-        if let Type::Path(t) = &**t {
-            if let Some(PathSegment {
-                arguments: PathArguments::AngleBracketed(t),
-                ..
-            }) = t.path.segments.first()
-            {
-                if let Some(GenericArgument::Type(Type::Path(t))) = t.args.first() {
-                    if let Some(t) = t.path.get_ident() {
-                        return Ok(t);
-                    }
-                }
-            }
-        }
-    }
-    Err(syn::Error::new(
-        f.sig.ident.span(),
-        "expected function to return `Server<ServerType>` instance",
-    )
-    .into_compile_error()
-    .into())
-}
-
-/// Generates code to derive Clone on a user supplied `ServerType`.
-fn gen_derive(server_def: &ItemStruct) -> TokenStream2 {
-    quote!(
-        #[::std::prelude::v1::derive(::std::clone::Clone)]
-        #server_def
-    )
-}
-
-/// Generates the main function for the given mentat implementation.
-fn gen_main(
-    server_call: &TokenStream2,
-    server_type: &Ident,
-    cache_type: Option<&Ident>,
-) -> TokenStream2 {
-    let routes = route_builder::build_routes(server_type, cache_type);
-
-    quote!(
-        use ::mentat::{conf::NodePid, macro_exports::tokio, sysinfo::Pid};
-        #[tokio::main]
-        async fn main() {
-            use ::mentat::macro_exports::*;
-            let server = #server_call;
-            let mut app = Router::new();
-            #routes
-            server.serve(app).await
-        }
-    )
-}
+use syn::{self, parse_macro_input, AttributeArgs, ItemFn, ItemStruct};
 
 /// A macro for generating mentat routes from a default `Server` instance.
 #[proc_macro_attribute]
