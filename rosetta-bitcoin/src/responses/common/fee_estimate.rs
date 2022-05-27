887b436b987b531a8200e6ae4edfd23f1795ab2f
0rphon <59403052+0rphon@users.noreply.github.com>
2022-05-27 14:23:08 -0700

    removed repos

--- rosetta-bitcoin/src/responses/common/fee_estimate.rs
+++
@@ -1,9 +0,0 @@
-use mentat::serde::Deserialize;
-
-#[derive(Clone, Debug, Deserialize)]
-#[serde(crate = "mentat::serde")]
-pub struct FeeEstimate {
-    pub feerate: f64,
-    pub errors: Vec<String>,
-    pub blocks: usize,
-}
--- tools/rosetta-cli-conf/run.sh
+++
@@ -1,2 +0,0 @@
-./rosetta-cli-bin check:data --configuration-file rosetta-cli-conf/bitcoin_testnet/config.json
-./rosetta-cli-bin check:construction --configuration-file rosetta-cli-conf/bitcoin_testnet/config.json
