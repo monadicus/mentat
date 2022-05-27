13a829bef7fc782c4057a1244cea5eab9f20b0d0
mowkoshka <99986223+mowkoshka@users.noreply.github.com>
2022-05-27 11:51:23 -0700

    Feature/health check node connections (#96)
    * add initial support for number of node connections
    
    * updated health check responses for node, started working on health check impls for snarkos and btc
    
    * addded node information for btc, snarkos doesn't seem to have most this information

--- mentat/src/api/optional.rs
+++ mentat/src/api/optional.rs
@@ -15,6 +15,8 @@ pub trait OptionalApi: Clone + Default {
     async fn health(
         &self,
         caller: Caller,
+        mode: &Mode,
+        rpc_caller: RpcCaller,
         server_pid: Pid,
         node_pid: NodePid,
     ) -> MentatResponse<HealthCheckResponse> {
@@ -24,7 +26,12 @@ pub trait OptionalApi: Clone + Default {
             caller,
             msg: "Healthy!".to_string(),
             usage: self.usage("server", &system, server_pid).await?,
-            node_usage: self.usage("node", &system, node_pid.0).await?,
+            node: NodeInformation {
+                usage: self.usage("node", &system, node_pid.0).await?,
+                address: self.node_address(&rpc_caller).await?,
+                connections: self.node_connections(mode, &rpc_caller).await?,
+                net_usage: self.node_net_usage(mode, &rpc_caller).await?,
+            },
             cache_usage: self.check_cache_usage().await?,
         }))
     }
@@ -45,6 +52,29 @@ pub trait OptionalApi: Clone + Default {
         })
     }
 
+    /// A method for getting the address of the node.
+    async fn node_address(&self, _rpc_caller: &RpcCaller) -> Result<String> {
+        Ok(String::new())
+    }
+
+    /// A method for getting the number of connections a node has.
+    async fn node_connections(
+        &self,
+        _mode: &Mode,
+        _rpc_caller: &RpcCaller,
+    ) -> Result<Option<NodeConnections>> {
+        Ok(None)
+    }
+
+    /// A method for getting the network usage of a node.
+    async fn node_net_usage(
+        &self,
+        _mode: &Mode,
+        _rpc_caller: &RpcCaller,
+    ) -> Result<Option<NodeNetwork>> {
+        Ok(None)
+    }
+
     /// A default implementation for providing a cache usage check.
     async fn check_cache_usage(&self) -> Result<Option<Usage>> {
         Ok(None)
@@ -59,6 +89,8 @@ impl OptionalApi for UnimplementedOptionalApi {
     async fn health(
         &self,
         _caller: Caller,
+        _mode: &Mode,
+        _rpc_caller: RpcCaller,
         _server_pid: Pid,
         _node_pid: NodePid,
     ) -> MentatResponse<HealthCheckResponse> {
--- rosetta-snarkos/src/optional_api.rs
+++ rosetta-snarkos/src/optional_api.rs
@@ -1,7 +1,11 @@
-use mentat::{api::OptionalApi, axum::async_trait};
+use mentat::{api::OptionalApi, axum::async_trait, errors::Result, server::RpcCaller};
 
 #[derive(Clone, Default)]
 pub struct SnarkosOptionalApi;
 
 #[async_trait]
-impl OptionalApi for SnarkosOptionalApi {}
+impl OptionalApi for SnarkosOptionalApi {
+    async fn node_address(&self, _rpc_caller: &RpcCaller) -> Result<String> {
+        Ok("unknown".to_string())
+    }
+}
