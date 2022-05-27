36d05f669e12c06be8bcd4a4724eccef3e4335d2
MACS-J1149 <e@monadic.us>
2022-05-27 14:23:08 -0700

    removed repos

--- rosetta-bitcoin/regtest/node_bash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# run bash scripts on the regtest node
-docker exec -i bitcoin_regtest su bitcoin -c bash
--- rosetta-bitcoin/src/responses/data/mod.rs
+++
@@ -1,7 +0,0 @@
-use mentat::serde::Deserialize;
-
-mod getblock;
-pub use getblock::*;
-
-mod getblockchaininfo;
-pub use getblockchaininfo::*;
