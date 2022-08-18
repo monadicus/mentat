5ba53b26acd6021a69b0afa8ebd4cefdf2c16a25
0rphon <59403052+0rphon@users.noreply.github.com>
2022-08-18 14:14:34 -0700

    error tests

--- crates/mentat-syncer/src/errors_test.rs
+++ crates/mentat-syncer/src/errors_test.rs
@@ -1,5 +1,25 @@
-//! TODO
-/// todo
-fn todo() {
-    todo!()
+#![allow(clippy::missing_docs_in_private_items)]
+
+use std::error::Error;
+
+use mentat_test_utils::TestCase;
+
+use crate::errors::{err, SyncerError};
+
+#[test]
+fn test_err() {
+    let tests = vec![
+        TestCase {
+            name: "is a keys error",
+            payload: Box::new(SyncerError::CannotRemoveGenesisBlock) as Box<dyn Error>,
+            criteria: true,
+        },
+        TestCase {
+            name: "not a keys error",
+            payload: "blah".into(),
+            criteria: false,
+        },
+    ];
+
+    TestCase::run_output_match(tests, err)
 }
--- crates/mentat-syncer/src/lib.rs
+++ crates/mentat-syncer/src/lib.rs
@@ -5,8 +5,10 @@
 #![warn(clippy::todo)]
 
 pub mod errors;
+#[cfg(test)]
 pub mod errors_test;
 // mod golang;
 pub mod syncer;
+#[cfg(test)]
 pub mod syncer_test;
 pub mod types;
